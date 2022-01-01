0e53623710975a56b703b1a4618f52f1fe440095
danielwilczak <danielwilczak101@gmail.com>
2022-01-01 02:13:26 -0500

    At 1:03:22 point

--- angular-api/src/app/components/header/header.component.ts
+++ angular-api/src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title: string = 'OrderWallet API';
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleAddTask(){
+    console.log('toggle');
   }
 
 }
---
+++ angular-api/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import {Task} from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+  
+  constructor(private taskSerivce: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskSerivce.getTasks().subscribe((tasks) => 
+      (this.tasks = tasks));
+  }
+}
